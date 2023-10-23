641242a5a4f11c26b834306d2d80440ece0c2295
Гоша <simonov.gm@phystech.edu>
2023-10-23 22:53:27 +0300

    dozdacha

--- 2-1-running-light.py
+++ 2-1-running-light.py
@@ -1,12 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-leds=[]
+leds=[2, 3, 4, 17, 27, 22, 10, 9]
 GPIO.setup(leds, GPIO.OUT)
 while(1):
  N=int(input('количество повторов='))
-    for j in range(N):
-        for i in range(leds):
+for j in range(N):
+        for i in range(8):
             GPIO.output(leds[i],1)
             time.sleep(0.2)
             GPIO.output(leds[i],0)
